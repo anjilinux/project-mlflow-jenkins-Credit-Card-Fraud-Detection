b6bfe35a55af3e3af133b51c522bc44b7e4f545f
anjilinux <anjireddy2024@gmail.com>
2026-02-02 22:08:27 +0530

    docker-change

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,18 @@
-FROM python:3.10-slim
+FROM python:3.12-slim
 
+# Set working directory
 WORKDIR /app
 
+# Copy requirements and install
 COPY requirements.txt .
+RUN python -m pip install --upgrade pip
 RUN pip install --no-cache-dir -r requirements.txt
 
+# Copy all code + model
 COPY . .
 
-CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8005"]
+# Expose FastAPI port
+EXPOSE 8005
+
+# Start FastAPI
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8005"]
--- main.py
+++ main.py
@@ -5,24 +5,22 @@ from schema import FraudInputSchema
 
 app = FastAPI(title="Credit Card Fraud Detection")
 
-# Load trained model
+# Load model
 model = joblib.load("model.pkl")
-
 
 @app.get("/health")
 def health():
     return {"status": "ok"}
 
-
 @app.post("/predict")
-def predict(data: FraudInputSchema):
-    # Convert Pydantic model → numpy array
-    features = np.array([list(data.model_dump().values())])
-
+def predict(transaction: FraudInputSchema):
+    # Convert Pydantic model to array
+    data_array = np.array([list(transaction.model_dump().values())])
+    
     # Predict
-    prediction = int(model.predict(features)[0])
+    prediction = model.predict(data_array)[0]  # 0 = Legit, 1 = Fraud
 
     return {
-        "fraud": prediction,
+        "fraud": int(prediction),
         "label": "Fraud" if prediction == 1 else "Legit"
     }
